7ed6f80058a9f2554bd513063bf969179280ab22
Arian <arian.kulmer@web.de>
2025-08-27 15:44:50 +0200

    Revert "display-drivers: mi_disp: Only enable debugfs if supported"
    This reverts commit b573bd3e39ae504e6046bd449dc2e0d10612d95c.

--- qcom/opensource/display-drivers/msm/mi_disp/mi_sde_connector.c
+++ qcom/opensource/display-drivers/msm/mi_disp/mi_sde_connector.c
@@ -149,7 +149,6 @@ int mi_sde_connector_register_esd_irq(struct sde_connector *c_conn)
 	return rc;
 }
 
-#if MI_DISP_DEBUGFS_ENABLE
 int mi_sde_connector_debugfs_esd_sw_trigger(void *display)
 {
 	struct dsi_display *dsi_display = (struct dsi_display *)display;
@@ -212,7 +211,7 @@ int mi_sde_connector_debugfs_esd_sw_trigger(void *display)
 
 	return 0;
 }
-#endif
+
 
 static int mi_sde_connector_update_aod_status(struct drm_connector *connector,
 		bool is_aod_exit)
